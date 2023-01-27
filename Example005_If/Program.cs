e67cb869348dfa05fbdc0c81e5840c587c7f81d3
Elizaveta <>
2023-01-27 18:23:46 +0300

    add example005

---
+++ Example005_If/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.readline();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
